80a77e20f80ec92abe2407e862a8a12f0fd86886
t1meofmy3nd <161883340+t1meofmy3nd@users.noreply.github.com>
2024-08-17 21:27:58 +0400

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,16 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for str in range(n):
+        string = []
+        for col in range(m):
+            string.append(value)
+        matrix.append(string)
+    return matrix
+
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
